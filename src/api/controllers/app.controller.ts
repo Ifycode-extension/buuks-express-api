4c94b9d86d0b116158ca6bd03cabb0e96719e35c
Ifycode <marychord@gmail.com>
2022-03-06 21:30:13 +0100

    Make req.body work and install other needed dependencies

--- src/api/controllers/app.controller.ts
+++ src/api/controllers/app.controller.ts
@@ -1,16 +1,8 @@
 import { Request, Response } from 'express';
 
-let routeName = 'demo';
-let item = `${routeName}-item`;
-
 export const getAppHandler = async (req: Request, res: Response) => {
-  try {
-    //let demo = await getDemoItems();
-    res.status(200).send('yay!');
-    //return demo;
-    console.log(`\nGET request successful! \n\nRunning at http://localhost:3000/${routeName}/\n`);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send(err.message);
-  }
+  // console.log(req.body);
+  res.status(200).json({
+    message: 'App works!'
+  });
 }
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
 import { router as appController } from './api/routes/appRoute';
 
+dotenv.config();
+
 const app: Express = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+app.use(cors({ origin: `http://localhost:${process.env.CLIENT_PORT}` }));
 
 app.use('/', appController);
 
