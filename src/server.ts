57e289bb2117ea446b7c88d1a169e28ded2f8155
Ifycode <marychord@gmail.com>
2022-03-06 04:37:08 +0100

    Fix: esmodule syntax error (temporary fix)

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import { app } from './app';
-import chalk from 'chalk';
+//import chalk from 'chalk';
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   try {
-    console.log(chalk.cyanBright(`\nServer running at http://localhost:${port}`));
+    console.log(`\nServer running at http://localhost:${port}`);
+    //console.log(chalk.cyanBright(`\nServer running at http://localhost:${port}`));
   } catch (err) {
     console.log(err);
   }
 });
+
+// Chalk import is what causes the import error
+// Also had to use node v16 (was using v14 before - not sure if this is a cause of the issue though...)
