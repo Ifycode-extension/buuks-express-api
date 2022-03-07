43afb0ab97bd69589706d04be260a334d3867f16
Ifycode <marychord@gmail.com>
2022-03-08 00:41:08 +0100

    Add config for validating resources with zod

---
+++ src/middleware/validate.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+
+const validateResource = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
+  try {
+    schema.parse({
+      body: req.body,
+      query: req.query,
+      params: req.params
+    });
+  } catch (err: any) {
+    return res.status(400).send(err.errors);
+  }
+}
+
+export default validateResource;
+
+// TODO: reformat the function returning a function so that it looks a bit different
+// TODO: Is it possiible to use .json() instead of .send() in the catch block?
