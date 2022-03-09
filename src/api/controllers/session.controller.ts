0b5e8aec214260252f6aa2d7072929c78b5d1399
Ifycode <marychord@gmail.com>
2022-03-09 20:51:48 +0100

    Add: jwt code in createSessionControllerHandler

--- src/api/controllers/session.controller.ts
+++ src/api/controllers/session.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { signJwt } from '../../utils/jwt.utils';
 import { createSessionService } from '../services/session.service';
 import { validatePasswordService } from '../services/user.service';
 
@@ -16,4 +17,22 @@ export const createSessionControllerHandler = async (req: Request, res: Response
 
   // create a session
   const session = await createSessionService(user._id, req.get('user-agent') || '');
+
+  // create an access token
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: `${process.env.ACCESSTOKEN_TTL}` } // will live for the duration of e.g. minutes specified in the .env file
+  );
+
+  // create an refresh token
+  const refreshToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: `${process.env.ACCESSTOKEN_TTL}` } // will live for the duration of e.g. year specified in the .env file
+  );
+
+  //return access and refresh token
+  return res.status(200).json({
+    accessToken,
+    refreshToken
+  });
 }
