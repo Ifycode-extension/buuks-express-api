52985924040509cc37dfa102b735ebc17874012b
Ifycode <marychord@gmail.com>
2022-03-11 18:26:47 +0100

    Update Book Schema with user and pdf properties, iinterface with also created at and updated at

--- src/api/models/book.model.ts
+++ src/api/models/book.model.ts
@@ -1,17 +1,24 @@
 import mongoose from 'mongoose';
+import { UserDocument } from './user.model';
 
 export interface BookDocument extends mongoose.Document {
+  user: UserDocument['_id'];
   title: string;
   description: string;
+  pdf: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const collectionName = 'book';
 
 const BookSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   title: { type: String, required: true },
-  description: { type: String, required: true }
+  description: { type: String, required: true },
+  pdf: { type: String, required: true }
 });
 
-const BookModel = mongoose.model(collectionName, BookSchema, collectionName); //declare collection name a second time to prevent mongoose from pluralizing or adding 's' to the collection name
+const BookModel = mongoose.model<BookDocument>(collectionName, BookSchema, collectionName); //declare collection name a second time to prevent mongoose from pluralizing or adding 's' to the collection name
 
 export { BookModel };
