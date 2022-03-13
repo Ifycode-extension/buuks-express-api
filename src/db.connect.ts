f1d36b1f3b5421bc33a5f364884d79cdacfbca50
Ifycode <marychord@gmail.com>
2022-03-13 07:14:34 +0100

    Add: MongoDB atlas connection

--- src/db.connect.ts
+++ src/db.connect.ts
@@ -6,7 +6,7 @@ dotenv.config();
 async function mongooseConnect() {
   try {
     mongoose.connect(`${process.env.MONGODB_URI}`);
-    console.log('\nInstalled MongoDB connection successful!!!');
+    console.log('\nConnection to mongoDB successful!!!');
   } catch (err) {
     console.log(`\nError in DB connection: ${err.message} \n`);
   }
