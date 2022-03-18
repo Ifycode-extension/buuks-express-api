b5f92d7150131fbb530498f2fd38132bbd7f8303
Ifycode <marychord@gmail.com>
2022-03-18 22:44:57 +0100

    Add readme message to app controller

--- src/api/controllers/app.controller.ts
+++ src/api/controllers/app.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 export const getAppController = async (req: Request, res: Response) => {
   res.status(200).json({
-    message: 'App works!'
+    message: 'Buuks API!',
+    readme: 'See readme at https://github.com/Ifycode/buuks-api-expressjs for how buuks API works'
   });
 }
