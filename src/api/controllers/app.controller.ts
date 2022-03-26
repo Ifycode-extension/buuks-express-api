86b180417ea31fddb4ced90a7a0a2deff0698387
Ifycode <marychord@gmail.com>
2022-03-27 00:28:46 +0100

    Fix App controller response

--- src/api/controllers/app.controller.ts
+++ src/api/controllers/app.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 
 export const getAppController = async (req: Request, res: Response) => {
   res.status(200).json({
-    message: 'Buuks API!',
-    readme: 'See readme at https://github.com/Ifycode/buuks-express-api for how buuks API works'
+    message: 'Welcome to Buuks API! Find links to project readme and client below.',
+    readme: 'https://github.com/Ifycode/buuks-express-api',
+    client: 'https://buuks.netlify.app'
   });
 }
