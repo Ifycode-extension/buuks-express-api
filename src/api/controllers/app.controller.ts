72255677b7e4b9ad3d119bb5135751c303db0183
Ifycode <marychord@gmail.com>
2022-03-26 23:56:31 +0100

    Update controller with new repo name

--- src/api/controllers/app.controller.ts
+++ src/api/controllers/app.controller.ts
@@ -3,6 +3,6 @@ import { Request, Response } from 'express';
 export const getAppController = async (req: Request, res: Response) => {
   res.status(200).json({
     message: 'Buuks API!',
-    readme: 'See readme at https://github.com/Ifycode/buuks-api-expressjs for how buuks API works'
+    readme: 'See readme at https://github.com/Ifycode/buuks-express-api for how buuks API works'
   });
 }
